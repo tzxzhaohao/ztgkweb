4bcc1f480647a216a9398d83ebe2e1c58eeb56bd
zhaohao4 <1104270550@qq.com>
2023-07-14 13:50:30 +0800

    fix: 海洋图

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,123 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three'
+import { Water } from 'three/addons/objects/Water.js'
+import { Sky } from 'three/addons/objects/Sky.js'
+import WaterNormals from './assets/waternormals.jpg?url'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let camera: THREE.PerspectiveCamera,
+  scene: THREE.Scene,
+  renderer: THREE.WebGLRenderer,
+  water: Water,
+  sun: THREE.Vector3
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+init()
+animate()
+
+function init() {
+  const container = document.getElementById('app') as HTMLDivElement
+  renderer = new THREE.WebGLRenderer()
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.useLegacyLights = false
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
+  renderer.toneMappingExposure = 0.5
+  container.appendChild(renderer.domElement)
+
+  //
+
+  scene = new THREE.Scene()
+
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  )
+  camera.position.set(30, 30, 100)
+
+  //
+
+  sun = new THREE.Vector3()
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000)
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      WaterNormals,
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  })
+
+  water.rotation.x = -Math.PI / 2
+
+  scene.add(water)
+
+  // Skybox
+
+  const sky = new Sky()
+  sky.scale.setScalar(10000)
+  scene.add(sky)
+
+  const skyUniforms = sky.material.uniforms
+
+  skyUniforms['turbidity'].value = 10
+  skyUniforms['rayleigh'].value = 2
+  skyUniforms['mieCoefficient'].value = 0.005
+  skyUniforms['mieDirectionalG'].value = 0.8
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180,
+  }
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer)
+  let renderTarget: THREE.WebGLRenderTarget<THREE.Texture> | undefined
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation)
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth)
+
+    sun.setFromSphericalCoords(1, phi, theta)
+
+    sky.material.uniforms['sunPosition'].value.copy(sun)
+    water.material.uniforms['sunDirection'].value.copy(sun).normalize()
+
+    if (renderTarget !== undefined) renderTarget.dispose()
+
+    renderTarget = pmremGenerator.fromScene(sky)
+
+    scene.environment = renderTarget.texture
+  }
+
+  updateSun()
+
+  window.addEventListener('resize', onWindowResize)
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  render()
+}
+
+function render() {
+  water.material.uniforms['time'].value += 1.0 / 60.0
+  renderer.render(scene, camera)
+}
